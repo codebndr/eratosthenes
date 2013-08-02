2b682c538a076fb6244896b9e5ae8a50259afefd
Vasileios Georgitzikis <tzikis@gmail.com>
2013-08-01 20:02:35 -0700

    added authorization, http monitoring, http test monitoring, and some extremely basic tests

--- Symfony/src/Codebender/LibraryBundle/Controller/DefaultController.php
+++ Symfony/src/Codebender/LibraryBundle/Controller/DefaultController.php
@@ -10,94 +10,140 @@ use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
-	public function listAction()
-    {
-		$arduino_library_files = $this->container->getParameter('arduino_library_directory')."/";
-
-	    $finder = new Finder();
-	    $finder2 = new Finder();
-	    $finder3 = new Finder();
-
-	    $finder->files()->name('*.ino')->name('*.pde');
-	    $finder2->files()->name('*.ino')->name('*.pde');
-	    $finder3->files()->name('*.ino')->name('*.pde');
-
-	    $built_examples = array();
-	    if (is_dir($arduino_library_files."examples"))
-	    {
-		    $finder->in($arduino_library_files."examples");
-		    $built_examples = $this->iterateDir($finder);
-	    }
-
-	    $included_libraries = array();
-	    if(is_dir($arduino_library_files."libraries"))
-	    {
-		    $finder2->in($arduino_library_files."libraries");
-		    $included_libraries = $this->iterateDir($finder2);
-	    }
-
-	    $external_libraries = array();
-	    if (is_dir($arduino_library_files."external-libraries"))
-	    {
-		    $finder3->in($arduino_library_files."external-libraries");
-		    $external_libraries = $this->iterateDir($finder3);
-	    }
-
-
-	    ksort($built_examples);
-	    ksort($included_libraries);
-	    ksort($external_libraries);
-
-	    return new Response(json_encode(array("success" => true,
-									"text" => "Successful Request!",
-							  "categories" => array("Examples" => $built_examples,
-										   "Builtin Libraries" => $included_libraries,
-					                      "External Libraries" => $external_libraries))));
-
-    }
-
-	public function getCodeAction()
+	public function statusAction()
 	{
-		$arduino_library_files = $this->container->getParameter('arduino_library_directory')."/";
-
-		$finder = new Finder();
-
-		$request = Request::createFromGlobals();
-
-		// retrieve GET and POST variables respectively
-		$file = $request->query->get('file');
-
-		$last_slash = strrpos($file, "/");
-		$filename = substr($file, $last_slash+1);
-		$directory = substr($file, 0, $last_slash);
-
-		$finder->files()->name($filename);
-		if (is_dir($arduino_library_files."examples"))
-		{
-			$finder->in($arduino_library_files."examples");
-		}
-
-		if (is_dir($arduino_library_files."libraries"))
-		{
-			$finder->in($arduino_library_files."libraries");
-		}
-
-		if (is_dir($arduino_library_files."external-libraries"))
-		{
-			$finder->in($arduino_library_files."external-libraries");
-		}
-
-		$finder->path($directory);
-
-		$response = "";
-		foreach($finder as $file)
-		{
-			$response = $file->getContents();
-		}
-		return new Response($response);
+		return new Response(json_encode(array("success" => true, "status" => "OK")));
 	}
 
-	private function iterateDir($finder)
+	public function testAction($auth_key)
+	{
+		if ($auth_key !== $this->container->getParameter('auth_key'))
+		{
+			return new Response(json_encode(array("success" => false, "message" => "Invalid authorization key.")));
+		}
+
+		set_time_limit(0); // make the script execution time unlimited (otherwise the request may time out)
+
+		// change the current Symfony root dir
+		chdir($this->get('kernel')->getRootDir()."/../");
+
+		//TODO: replace this with a less horrible way to handle phpunit
+		exec("phpunit -c app --stderr 2>&1", $output, $return_val);
+
+		return new Response(json_encode(array("success" => (bool) !$return_val, "message" => implode("\n", $output))));
+	}
+
+	public function listAction($auth_key, $version)
+    {
+	    if ($auth_key !== $this->container->getParameter('auth_key'))
+	    {
+		    return new Response(json_encode(array("success" => false, "step" => 0, "message" => "Invalid authorization key.")));
+	    }
+
+	    if ($version == "v1")
+	    {
+		    $arduino_library_files = $this->container->getParameter('arduino_library_directory')."/";
+
+		    $finder = new Finder();
+		    $finder2 = new Finder();
+		    $finder3 = new Finder();
+
+		    $finder->files()->name('*.ino')->name('*.pde');
+		    $finder2->files()->name('*.ino')->name('*.pde');
+		    $finder3->files()->name('*.ino')->name('*.pde');
+
+		    $built_examples = array();
+		    if (is_dir($arduino_library_files."examples"))
+		    {
+			    $finder->in($arduino_library_files."examples");
+			    $built_examples = $this->iterateDir($finder, "v1");
+		    }
+
+		    $included_libraries = array();
+		    if (is_dir($arduino_library_files."libraries"))
+		    {
+			    $finder2->in($arduino_library_files."libraries");
+			    $included_libraries = $this->iterateDir($finder2, "v1");
+		    }
+
+		    $external_libraries = array();
+		    if (is_dir($arduino_library_files."external-libraries"))
+		    {
+			    $finder3->in($arduino_library_files."external-libraries");
+			    $external_libraries = $this->iterateDir($finder3, "v1");
+		    }
+
+
+		    ksort($built_examples);
+		    ksort($included_libraries);
+		    ksort($external_libraries);
+
+		    return new Response(json_encode(array("success" => true,
+			    "text" => "Successful Request!",
+			    "categories" => array("Examples" => $built_examples,
+				    "Builtin Libraries" => $included_libraries,
+				    "External Libraries" => $external_libraries))));
+	    }
+	    else
+	    {
+		    return new Response(json_encode(array("success" => false, "step" => 0, "message" => "Invalid API version.")));
+	    }
+    }
+
+	public function getCodeAction($auth_key, $version)
+	{
+		if ($auth_key !== $this->container->getParameter('auth_key'))
+		{
+			return new Response(json_encode(array("success" => false, "step" => 0, "message" => "Invalid authorization key.")));
+		}
+
+		if ($version == "v1")
+		{
+			$arduino_library_files = $this->container->getParameter('arduino_library_directory')."/";
+
+			$finder = new Finder();
+
+			$request = Request::createFromGlobals();
+
+			// retrieve GET and POST variables respectively
+			$file = $request->query->get('file');
+
+			$last_slash = strrpos($file, "/");
+			$filename = substr($file, $last_slash + 1);
+			$directory = substr($file, 0, $last_slash);
+
+			$finder->files()->name($filename);
+			if (is_dir($arduino_library_files."examples"))
+			{
+				$finder->in($arduino_library_files."examples");
+			}
+
+			if (is_dir($arduino_library_files."libraries"))
+			{
+				$finder->in($arduino_library_files."libraries");
+			}
+
+			if (is_dir($arduino_library_files."external-libraries"))
+			{
+				$finder->in($arduino_library_files."external-libraries");
+			}
+
+			$finder->path($directory);
+
+			$response = "";
+			foreach ($finder as $file)
+			{
+				$response = $file->getContents();
+			}
+			return new Response($response);
+		}
+		else
+		{
+			return new Response(json_encode(array("success" => false, "step" => 0, "message" => "Invalid API version.")));
+		}
+	}
+
+	private function iterateDir($finder, $version)
 	{
 		$libraries = array();
 
@@ -118,7 +164,7 @@ class DefaultController extends Controller
 			$path = str_ireplace("/examples/", "/", $file->getRelativePath());
 			$library_name = strtok($path, "/");
 			$example_name = strtok("/");
-			$url = $this->get('router')->generate('codebender_library_getcode', array(),true).'?file='.$file->getRelativePathname();
+			$url = $this->get('router')->generate('codebender_library_getcode', array("auth_key" => $this->container->getParameter('auth_key'),"version" => $version),true).'?file='.$file->getRelativePathname();
 
 			if(!isset($libraries[$library_name]))
 			{
--- Symfony/src/Codebender/LibraryBundle/Tests/Controller/DefaultControllerTest.php
+++ Symfony/src/Codebender/LibraryBundle/Tests/Controller/DefaultControllerTest.php
@@ -6,12 +6,56 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class DefaultControllerTest extends WebTestCase
 {
-    public function testIndex()
-    {
-        $client = static::createClient();
+	public function testStatus()
+	{
+		$client = static::createClient();
 
-        $crawler = $client->request('GET', '/hello/Fabien');
+		$client->request('GET', '/status');
 
-        $this->assertTrue($crawler->filter('html:contains("Hello Fabien")')->count() > 0);
-    }
+		$this->assertEquals($client->getResponse()->getContent(), '{"success":true,"status":"OK"}');
+
+	}
+
+	public function testInvalidKey()
+	{
+		$client = static::createClient();
+
+		$client->request('GET', '/inValidKey/v1');
+
+		$this->assertEquals($client->getResponse()->getContent(), '{"success":false,"step":0,"message":"Invalid authorization key."}');
+
+	}
+
+	public function testInvalidAPI()
+	{
+		$client = static::createClient();
+
+		$auth_key = $client->getContainer()->getParameter("auth_key");
+
+		$client->request('GET', '/'.$auth_key.'/v666');
+
+		$this->assertEquals($client->getResponse()->getContent(), '{"success":false,"step":0,"message":"Invalid API version."}');
+
+	}
+
+	public function testIncorrectInputs()
+	{
+		$this->markTestIncomplete("No tests for invalid inputs yet");
+	}
+
+	public function testList()
+	{
+		$client = static::createClient();
+
+		$auth_key = $client->getContainer()->getParameter("auth_key");
+
+		$client->request('GET', '/'.$auth_key.'/v1');
+
+		$this->assertTrue(false, "There are no tests yet.");
+	}
+
+	public function testGetFile()
+	{
+		$this->assertTrue(false, "There are no tests yet.");
+	}
 }
