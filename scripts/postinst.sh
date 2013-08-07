564a72c79cd23c4d5209ba1f40d2b755c048dccd
Vasilis Georgitzikis <tzikis@gmail.com>
2013-08-07 22:17:07 +0000

    removed unused script code

--- scripts/postinst.sh
+++ scripts/postinst.sh
@@ -12,17 +12,6 @@ cd /opt/codebender/@PACKAGENAME@/Symfony
 sudo curl -s http://getcomposer.org/installer | sudo php
 sudo php composer.phar install
 
-#find a way to edit the fstab here
-#more info: https://help.ubuntu.com/community/FilePermissionsACLs
-#sudo mount -o remount /
-#
-#sudo rm -rf /opt/codebender/@PACKAGENAME@/Symfony/app/cache/*
-#sudo rm -rf /opt/codebender/@PACKAGENAME@/Symfony/app/logs/*
-#
-#sudo setfacl -R -m u:www-data:rwX -m u:ubuntu:rwX /opt/codebender/@PACKAGENAME@/Symfony/app/cache /opt/codebender/@PACKAGENAME@/Symfony/app/logs
-#sudo setfacl -dR -m u:www-data:rwx -m u:ubuntu:rwx /opt/codebender/@PACKAGENAME@/Symfony/app/cache /opt/codebender/@PACKAGENAME@/Symfony/app/logs
-#sudo chown -R ubuntu:www-data /opt/codebender/@PACKAGENAME@/Symfony/app/cache/ /opt/codebender/@PACKAGENAME@/Symfony/app/logs
-#sudo chmod -R 775 /opt/codebender/@PACKAGENAME@/Symfony/app/cache/ /opt/codebender/@PACKAGENAME@/Symfony/app/logs
 
 sudo rm -rf /opt/codebender/@PACKAGENAME@/Symfony/app/cache/*
 sudo rm -rf /opt/codebender/@PACKAGENAME@/Symfony/app/logs/*
