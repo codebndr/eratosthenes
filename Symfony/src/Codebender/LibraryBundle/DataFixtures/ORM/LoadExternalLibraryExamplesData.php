1bca42caa5ae15f971f7de086e8832bc845f61a6
Fotis Papadopoulos <fot2803@hotmail.com>
2015-10-20 12:19:21 +0300

    Added first fixture class for external libraries' examples

---
+++ Symfony/src/Codebender/LibraryBundle/DataFixtures/ORM/LoadExternalLibraryExamplesData.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Codebender\LibraryBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Codebender\LibraryBundle\Entity\Example;
+
+/**
+ * Class LoadExternalLibraryExamplesData
+ * Provides default external library examples data for the database
+ *
+ * @package Codebender\LibraryBundle\DataFixtures\ORM
+ * @SuppressWarnings(PHPMD)
+ */
+class LoadExternalLibraryExamplesData extends AbstractFixture implements OrderedFixtureInterface
+{
+    /**
+     * Sets up fixture database data for external libraries' examples objects
+     *
+     * @param ObjectManager $objectManager
+     */
+    public function load(ObjectManager $objectManager)
+    {
+        /* @var \Codebender\LibraryBundle\Entity\ExternalLibrary $defaultLibrary */
+        $defaultLibrary = $this->getReference('defaultLibrary');
+
+        $defaultExample = new Example();
+        $defaultExample->setName('example_one');
+        $defaultExample->setLibrary($defaultLibrary);
+        $defaultExample->setPath('default/examples/example_one/example_one.ino');
+        $defaultExample->setBoards(null);
+
+        /*
+         * Set a reference for each example and add it to the database using
+         * the object manager interface
+         */
+        $this->setReference('defaultLibraryExample', $defaultExample);
+        $objectManager->persist($defaultExample);
+
+        /*
+         * After all fixture objects have been added to the ObjectManager (`persist` operation),
+         * it's time to flush the contents of the ObjectManager
+         */
+        $objectManager->flush();
+    }
+
+    /**
+     * Returns the order in which the current fixture will be loaded,
+     * compared to other fixture classes.
+     * Examples database data should be provided after the library data,
+     * because there is a foreign key constraint between them which should
+     * be satisfied.
+     *
+     * @return int
+     */
+    public function getOrder()
+    {
+        return 2;
+    }
+}
