9946c2a307a10b8ad9c13566bb2d8e0111d93ef0
Fotis Papadopoulos <fot2803@hotmail.com>
2015-10-23 11:58:58 +0300

    Renamed apache 2.4 configuration file

--- scripts/install_apache.sh
+++ scripts/install_apache.sh
@@ -5,7 +5,7 @@ set -e
 PACKAGENAME=eratosthenes
 
 # Copy the apache VirtualHost configuration file to the available site configurations directory
-sudo cp /opt/codebender/$PACKAGENAME/apache-config /etc/apache2/sites-available/codebender-$PACKAGENAME
+sudo cp /opt/codebender/$PACKAGENAME/apache-config-2.4 /etc/apache2/sites-available/codebender-$PACKAGENAME
 cd /etc/apache2/sites-enabled
 # Add the vhost configuration file to the enabled sites
 sudo ln -s ../sites-available/codebender-$PACKAGENAME 00-codebender.conf
