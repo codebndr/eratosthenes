3c279b0d9309c5a60d31b8349804038021e649f2
Fotis Papadopoulos <fot2803@hotmail.com>
2015-11-04 17:17:36 +0200

    Fixed an if statement

--- Symfony/src/Codebender/LibraryBundle/Handler/DefaultHandler.php
+++ Symfony/src/Codebender/LibraryBundle/Handler/DefaultHandler.php
@@ -589,7 +589,7 @@ class DefaultHandler
         $machineNames = array();
 
         foreach ($children as $child) {
-            if ($child['type'] == 'blob' || pathinfo($child['path'], PATHINFO_EXTENSION) == 'h') {
+            if ($child['type'] == 'blob' && pathinfo($child['path'], PATHINFO_EXTENSION) == 'h') {
                 $machineNames[] = pathinfo($child['path'], PATHINFO_FILENAME);
             }
         }
