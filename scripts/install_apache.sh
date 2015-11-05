88ec07b74576248a5a58c0c151cf0b5703e3d19e
Fotis Papadopoulos <fot2803@hotmail.com>
2015-11-05 15:51:05 +0200

    Added missing conf extension to the apache 2.4 config file

--- scripts/install_apache.sh
+++ scripts/install_apache.sh
@@ -5,7 +5,7 @@ set -e
 PACKAGENAME=eratosthenes
 
 # Copy the apache VirtualHost configuration file to the available site configurations directory
-sudo cp /opt/codebender/$PACKAGENAME/apache-config-2.4 /etc/apache2/sites-available/codebender-$PACKAGENAME
+sudo cp /opt/codebender/$PACKAGENAME/apache-config-2.4 /etc/apache2/sites-available/codebender-$PACKAGENAME.conf
 cd /etc/apache2/sites-enabled
 # Add the vhost configuration file to the enabled sites
 sudo a2ensite codebender-$PACKAGENAME
