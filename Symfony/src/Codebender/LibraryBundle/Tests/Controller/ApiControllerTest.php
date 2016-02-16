4d3ee1b6436f67de54f9dd68d717abc5b717598d
Thng Kai Yuan <hedonist.ky@gmail.com>
2016-02-17 00:22:17 +0800

    Add test for the checkGithubUpdates API

--- Symfony/src/Codebender/LibraryBundle/Tests/Controller/ApiControllerTest.php
+++ Symfony/src/Codebender/LibraryBundle/Tests/Controller/ApiControllerTest.php
@@ -25,6 +25,39 @@ class ApiControllerTest extends WebTestCase
     }
 
     /**
+     * This method tests the checkGithubUpdates API.
+     */
+    public function testCheckGithubUpdates()
+    {
+        $client = static::createClient();
+
+        $authorizationKey = $client->getContainer()->getParameter('authorizationKey');
+
+        $client = $this->postApiRequest($client, $authorizationKey, '{"type":"checkGithubUpdates"}');
+
+        $response = json_decode($client->getResponse()->getContent(), true);
+
+        $this->assertTrue($response['success']);
+        $this->assertEquals('1 external libraries need to be updated', $response['message']);
+        /*
+         * DynamicArrayHelper library's last commit is not the same as its origin.
+         */
+        $this->assertEquals('DynamicArrayHelper', $response['libraries'][0]['Machine Name']);
+
+        /*
+         * Disabling the library should make it not be returned in the list.
+         */
+        $handler = $this->getService('codebender_api.checkGithubUpdates');
+        $handler->toggleLibraryStatus('DynamicArrayHelper');
+        $client = $this->postApiRequest($client, $authorizationKey, '{"type":"checkGithubUpdates"}');
+
+        $response = json_decode($client->getResponse()->getContent(), true);
+
+        $this->assertTrue($response['success']);
+        $this->assertEquals('No external libraries need to be updated', $response['message']);
+    }
+
+    /**
      * Use this method for library manager API requests with POST data
      *
      * @param Client $client
@@ -54,5 +87,20 @@ class ApiControllerTest extends WebTestCase
         $client = $this->postApiRequest($client, $authorizationKey, '{"type":"' .$type. '"}');
         $response = json_decode($client->getResponse()->getContent(), true);
         return $response;
+    }
+
+    /**
+     * This method returns a given service from its name.
+     *
+     * @param $service
+     * @return the requested service
+     */
+    private function getService($service)
+    {
+        $kernel = static::createKernel();
+        $kernel->boot();
+
+        $container = $kernel->getContainer();
+        return $container->get($service);
     }
 }
