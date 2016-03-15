8802539ddccbefd7ba4808be821eae9ef25b7825
Jiang Sheng <gisonrg@gmail.com>
2016-03-15 18:08:53 +0800

    Create a new API to get default version of a library for a partner

---
+++ Symfony/src/Codebender/LibraryBundle/Handler/ApiCommand/GetDefaultVersionCommand.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Codebender\LibraryBundle\Handler\ApiCommand;
+
+use Codebender\LibraryBundle\Handler\ApiHandler;
+use Symfony\Component\Finder\Finder;
+
+class GetDefaultVersionCommand extends AbstractApiCommand
+{
+    public function execute($content)
+    {
+        if ($content['library'] === null) {
+            return ['success' => false, 'message' => 'Wrong data'];
+        }
+        $defaultHeader = $content['library'];
+
+        /* @var ApiHandler $handler */
+        $handler = $this->get('codebender_library.apiHandler');
+        // check library exists
+        if (!$handler->isExternalLibrary($defaultHeader, true)) {
+            return ['success' => false, 'message' => 'No library named ' . $defaultHeader . ' was found.'];
+        }
+        $version = $handler->fetchPartnerDefaultVersion($this->getRequest()->get('authorizationKey'), $defaultHeader);
+
+        return ['success' => true, 'version' => $version->getVersion()];
+    }
+}
